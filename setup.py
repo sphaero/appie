da00325e96c39991f1eb6c9b3cf426efb6732b92
Arnaud Loonstra <arnaud@sphaero.org>
2015-12-31 13:17:48 +0100

    added travis config and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name='appie',
+        version='0.5',
+        description='Static generator for dynamic websites',
+        author='Arnaud Loonstra',
+        author_email='arnaud@sphaero.org',
+        url='http://www.github.com/sphaero/appie/',
+        packages=['appie'],
+        scripts=['bin/appie'],
+        include_package_data=True,
+        requires=['textile', 'markdown', 'Pillow'],
+)
+
